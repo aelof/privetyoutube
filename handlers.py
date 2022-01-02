2c867d24dc8a2599898330272a278ed6a2a9d76d
alex <bestreality@mail.ru>
2022-01-02 16:19:04 +0300

    integrate youtube API

--- handlers.py
+++ handlers.py
@@ -1,10 +1,10 @@
+import os
+
 from telethon import events, Button
 from telethon.tl.custom import Message
 from gen import bot
-import requests
-from helpers import clean_video_link
-from time import sleep
-import os
+
+from helpers import clean_video_link, is_video_valid, get_download_link
 
 
 markup = [Button.text('скачать видео', single_use=True, resize=True)]
@@ -12,18 +12,20 @@ markup = [Button.text('скачать видео', single_use=True, resize=True)
 
 @bot.on(events.NewMessage(pattern='/start'))
 async def reload_command(event: Message):
-    await event.respond('Я проснулся и готов помочь тебе скачать видео, ориентируйся по кнопкам ниже )', buttons=markup)
+    await event.respond('Я проснулся и готов помочь скачать видео, ориентируйся по кнопкам ниже )', buttons=markup)
 
 
 @bot.on(events.NewMessage(func=lambda event: event.text.lower() == 'скачать видео'))
 async def download_video(event: Message):
     chat = await event.get_chat()
     async with bot.conversation(event.chat_id) as conv:
-        await conv.send_message('Отправьте ссылку с youtube: \n\nпример: "https://www.youtube.com/watch?v=ndizAUAR_Cs"', link_preview=False)
+        await conv.send_message('Отправьте youtube ссылку:\n\nпример: "https://www.youtube.com/watch?v=ndizAUAR_Cs"', link_preview=False)
         response = await conv.get_response()
-        link = response.text
-        if 'youtube' in link:
+        link = clean_video_link(response.text)
+        print(link)
+        if is_video_valid(link):
             message = await conv.send_message('Немного подождите, ваше видео готовится')
-            await conv.send_file(file=os.getcwd() + '/smile.png')
-            await message.edit('hello my dear with english')
-            await message.delete()
+            await conv.send_message(get_download_link(link))
+
+        else:
+            await conv.send_message('Похоже, что ссылка нерабочая')
--- helpers.py
+++ helpers.py
@@ -1,8 +1,21 @@
 import re
+import requests
+from pytube import YouTube 
+
+
 
 CLEANR = re.compile('<.*?>')
 
-def clean_video_link(raw_html):
-  cleantext = re.sub(CLEANR, '', raw_html)
-  return cleantext
 
+def clean_video_link(raw_html):
+    return re.sub(CLEANR, '', raw_html)
+   
+
+def is_video_valid(url):
+    r = requests.get(url)
+    return not "Video unavailable" in r.text
+
+
+def get_download_link(url):
+    yt = YouTube(url)
+    return yt.streams.get_by_itag(22).url
