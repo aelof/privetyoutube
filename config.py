76d1ae332399c290aea32fdfffbb7af855c571ae
alex <bestreality@mail.ru>
2022-01-26 20:04:29 +0300

    refactor link message handler

--- config.py
+++ config.py
@@ -8,3 +8,14 @@ BOT_TOKEN = os.getenv('BOT_TOKEN')
 API_ID = os.getenv('API_ID')
 API_HASH = os.getenv('API_HASH')
 
+
+
+
+
+
+
+
+
+
+
+
--- handlers.py
+++ handlers.py
@@ -7,25 +7,17 @@ from gen import bot
 from helpers import clean_video_link, is_video_valid, get_download_link
 
 
-markup = [Button.text('скачать видео', single_use=True, resize=True)]
-
-
 @bot.on(events.NewMessage(pattern='/start'))
 async def reload_command(event: Message):
-    await event.respond('Я проснулся и готов помочь скачать видео, ориентируйся по кнопкам ниже )', buttons=markup)
+    await event.respond('Я проснулся и готов помочь скачать видео, отправьте ссылку на него ')
 
 
-@bot.on(events.NewMessage(func=lambda event: event.text.lower() == 'скачать видео'))
+@bot.on(events.NewMessage(func=lambda event: 'youtube' in event.text))
 async def download_video(event: Message):
-    chat = await event.get_chat()
-    async with bot.conversation(event.chat_id) as conv:
-        await conv.send_message('Отправьте youtube ссылку:\n\nпример: "https://www.youtube.com/watch?v=ndizAUAR_Cs"', link_preview=False)
-        response = await conv.get_response()
-        link = clean_video_link(response.text)
-        print(link)
-        if is_video_valid(link):
-            message = await conv.send_message('Немного подождите, ваше видео готовится')
-            await conv.send_message(get_download_link(link))
-
-        else:
-            await conv.send_message('Похоже, что ссылка нерабочая')
+    link = clean_video_link(event.text)
+    if is_video_valid(link):
+        try:
+            await event.respond('Немного подождите, ваша ссылка проверяется')
+            await event.respond(f'Вот ваша ссылка: <a href="{get_download_link(link)}">ссылка на источник видео</a>')
+        except Exception as e:
+            await event.respond('Не удаётся сопоставить ссылку с видео, попробуйте другую')
